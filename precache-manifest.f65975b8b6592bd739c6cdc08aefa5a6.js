bc7a034f505c50b40526e57086d819f48e3cc37a
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2018-12-22 23:22:17 +0100

    Updates

---
+++ precache-manifest.f65975b8b6592bd739c6cdc08aefa5a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53410849894174b261d7",
+    "url": "/VirtualPiano/static/js/runtime~main.53410849.js"
+  },
+  {
+    "revision": "a28ab4f8e20eb2913392",
+    "url": "/VirtualPiano/static/js/main.a28ab4f8.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/VirtualPiano/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "a28ab4f8e20eb2913392",
+    "url": "/VirtualPiano/static/css/main.b85b2f92.chunk.css"
+  },
+  {
+    "revision": "d55d960137250fcb44fcc3b2b52e58f6",
+    "url": "/VirtualPiano/index.html"
+  }
+];
