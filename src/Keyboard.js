418e20eda8d9404ef3e29375f883e5387952dea0
Patrycja <madydapatrycja97@o2.pl>
2018-12-22 20:43:52 +0100

    Dodanie funkcjonalności - odgrywanie dźwięku po kliknięciu myszką na dany klawisz.

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,117 +1,91 @@
 import React from 'react';
 import './Keyboard.css';
 
+const PIANO_SOUNDS = 'sounds/piano_mp3/'
+
 class Keyboard extends React.Component {
+
+  play (event) {
+    const sound = event.target.id;
+
+    const audio = new Audio(`${PIANO_SOUNDS}${sound}.mp3`);
+    audio.play();
+  }
+
   render() {
     return (
       <ul className="Casing">
-        <li id="f3" className="white">
-          <audio></audio>
+        <li id="f3" className="white" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="fis3" className="black ">
-          <audio></audio>
+        <li id="fis3" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="g3" className="white l">
-          <audio></audio>
+        <li id="g3" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="gis3" className="black ">
-          <audio></audio>
+        <li id="gis3" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="a3" className="white l">
-          <audio></audio>
+        <li id="a3" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="ais3" className="black">
-          <audio></audio>
+        <li id="ais3" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="h3" className="white l">
-          <audio></audio>
+        <li id="h3" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="c4" className="white">
-          <audio></audio>
+        <li id="c4" className="white" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="cis4" className="black">
-          <audio></audio>
+        <li id="cis4" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="d4" className="white l">
-          <audio></audio>
+        <li id="d4" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="dis4" className="black ">
-          <audio></audio>
+        <li id="dis4" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="e4" className="white l">
-          <audio></audio>
+        <li id="e4" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="f4" className="white">
-          <audio></audio>
+        <li id="f4" className="white" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="fis4" className="black">
-          <audio></audio>
+        <li id="fis4" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="g4" className="white l">
-          <audio></audio>
+        <li id="g4" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="gis4" className="black ">
-          <audio></audio>
+        <li id="gis4" className="black " onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="a4" className="white l">
-          <audio></audio>
+        <li id="a4" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="ais4" className="black">
-          <audio></audio>
+        <li id="ais4" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="h4" className="white l">
-          <audio></audio>
+        <li id="h4" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="c5" className="white ">
-          <audio></audio>
+        <li id="c5" className="white " onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="cis5" className="black">
-          <audio></audio>
+        <li id="cis5" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="d5" className="white l">
-          <audio></audio>
+        <li id="d5" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="dis5" className="black">
-          <audio></audio>
+        <li id="dis5" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="e5" className="white l">
-          <audio></audio>
+        <li id="e5" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="f5" className="white">
-          <audio></audio>
+        <li id="f5" className="white" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="fis5" className="black">
-          <audio></audio>
+        <li id="fis5" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="g5" className="white l">
-          <audio></audio>
+        <li id="g5" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="gis5" className="black">
-          <audio></audio>
+        <li id="gis5" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="a5" className="white l">
-          <audio></audio>
+        <li id="a5" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="ais5" className="black">
-          <audio></audio>
+        <li id="ais5" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="h5" className="white l">
-          <audio></audio>
+        <li id="h5" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="c6" className="white">
-          <audio></audio>
+        <li id="c6" className="white" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="cis6" className="black">
-          <audio></audio>
+        <li id="cis6" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="d6" className="white l">
-          <audio></audio>
+        <li id="d6" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="dis6" className="black">
-          <audio></audio>
+        <li id="dis6" className="black" onMouseDown={(e) => this.play(e)}>
         </li>
-        <li id="e6" className="white l">
-          <audio></audio>
+        <li id="e6" className="white l" onMouseDown={(e) => this.play(e)}>
         </li>
       </ul>
     );
