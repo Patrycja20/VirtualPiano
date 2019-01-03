651eaf92a710673e5abc689b148035d996ff5ccd
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2019-01-03 23:08:29 +0100

    Pierwsze przymiarki do klikalnych dzwiekow z klawiatury

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,91 +1,107 @@
 import React from 'react';
 import './Keyboard.css';
-import { getFullSoundFolder } from './miniStore';
+import {
+  getFullSoundFolder,
+  addClickedSound,
+  removeClickedSound,
+  isSoundClicked,
+} from './miniStore';
 
 class Keyboard extends React.Component {
 
-  play (event) {
+  play(event) {
     const sound = event.target.id;
     let soundFolder = getFullSoundFolder();
 
     const audio = new Audio(`${soundFolder}${sound}.mp3`);
     audio.play();
+    addClickedSound(sound);
+    console.log(isSoundClicked(sound));
+  }
+
+  stop(event) {
+    const sound = event.target.id;
+    removeClickedSound(sound);
+    console.log(isSoundClicked(sound));
+
   }
 
   render() {
+    const aaa = `white ${isSoundClicked('f3') ? 'active' : null}`;
+    console.log('aaa', aaa);
     return (
       <ul className="Casing">
-        <li id="f3" className="white" onMouseDown={(e) => this.play(e)}>
+        <li id="f3" className={aaa} onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="fis3" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="fis3" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="g3" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="g3" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="gis3" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="gis3" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="a3" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="a3" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="ais3" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="ais3" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="h3" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="h3" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="c4" className="white" onMouseDown={(e) => this.play(e)}>
+        <li id="c4" className="white" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="cis4" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="cis4" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="d4" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="d4" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="dis4" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="dis4" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="e4" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="e4" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="f4" className="white" onMouseDown={(e) => this.play(e)}>
+        <li id="f4" className="white" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="fis4" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="fis4" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="g4" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="g4" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="gis4" className="black " onMouseDown={(e) => this.play(e)}>
+        <li id="gis4" className="black " onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="a4" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="a4" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="ais4" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="ais4" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="h4" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="h4" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="c5" className="white " onMouseDown={(e) => this.play(e)}>
+        <li id="c5" className="white " onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="cis5" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="cis5" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="d5" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="d5" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="dis5" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="dis5" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="e5" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="e5" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="f5" className="white" onMouseDown={(e) => this.play(e)}>
+        <li id="f5" className="white" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="fis5" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="fis5" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="g5" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="g5" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="gis5" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="gis5" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="a5" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="a5" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="ais5" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="ais5" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="h5" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="h5" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="c6" className="white" onMouseDown={(e) => this.play(e)}>
+        <li id="c6" className="white" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="cis6" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="cis6" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="d6" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="d6" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="dis6" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="dis6" className="black" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
-        <li id="e6" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="e6" className="white l" onMouseDown={this.play} onMouseUp={this.stop}>
         </li>
       </ul>
     );
--- src/miniStore.js
+++ src/miniStore.js
@@ -2,6 +2,7 @@ export const HARP = 'harp'; // Dzwiek harfy
 export const PIANO = 'piano';
 
 let sound = 'piano';
+let clickedArray = [];
 
 export function setSound(newSound) {
   sound = newSound;
@@ -21,4 +22,17 @@ export function setPianoSound() {
 
 export function setHarpSound() {
   sound = HARP;
+}
+
+
+export function addClickedSound(soundName) {
+  clickedArray.push(soundName);
+}
+
+export function removeClickedSound(soundName) {
+  clickedArray = clickedArray.filter(el => el !== soundName);
+}
+
+export function isSoundClicked(soundName) {
+  return clickedArray.filter(el => el === soundName).length !== 0;
 }
