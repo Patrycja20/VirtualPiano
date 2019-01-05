cff216c6bf6226e4a11cd79dc2887c0de321f261
Patrycja <madydapatrycja97@o2.pl>
2019-01-05 23:11:19 +0100

    Preloading dzwiekow

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import Keyboard from './Keyboard';
 import './App.css';
+import { mapKeysToNotes } from './shortcuts';
 
 class App extends React.Component {
+  initSounds() {
+    this.iterateSounds('piano');
+    this.iterateSounds('harp');
+  }
+
+  iterateSounds(name) {
+    const path = `sounds/${name}_mp3/`;
+    for (let i in mapKeysToNotes) {
+      const sound = mapKeysToNotes[i];
+      new Audio(`${path}${sound}.mp3`);
+    }
+  }
+
+  componentDidMount() {
+    this.initSounds();
+  }
 
   render() {
     return (
       <div>
         <div className="NameApp">
-        Virtual Piano
+          Virtual Piano
         </div>
         <Keyboard/>
       </div>
