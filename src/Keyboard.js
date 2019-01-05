04b15b4a587e196ae6dc8f034e05037cf8e01d0d
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2019-01-05 21:24:21 +0100

    Działające wciskanie klawiszy

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -18,8 +18,7 @@ class Keyboard extends React.Component {
     removeClickedSound = this.removeClickedSound;
 
     this.state = {
-      signatures: '0',
-      infoSignatures: 'Off',
+      signatures: false,
       keys: [],
       clickedPianoKeys: [],
       volume: ['On', 'On', 'On', 'On'],
@@ -34,41 +33,90 @@ class Keyboard extends React.Component {
     this.setState({clickedPianoKeys: this.state.clickedPianoKeys.filter(el => el !== soundName)})
   };
 
-  play(event) {
+  play = (event) => {
     const sound = event.target.id;
     let soundFolder = getFullSoundFolder();
 
     const audio = new Audio(`${soundFolder}${sound}.mp3`);
     audio.play();
-  }
+  };
 
   changeSignatures = (event) => {
-    if (event.target.value === '1') this.setState({signatures: '1', infoSignatures: 'On', keys: getKeys()});
-    else this.setState({signatures: '0', infoSignatures: 'Off', keys: []});
-  }
+    if (event.target.value === '1') this.setState({signatures: true, keys: getKeys()});
+    else this.setState({signatures: false, keys: []});
+  };
 
   onCheckedPiano = () => {
     setPianoSound();
-  }
+  };
 
   onCheckedHarp = () => {
     setHarpSound();
-  }
+  };
 
   onSetVolume = (event) => {
-    const newVolume = ['On', 'On', 'On', 'On']
+    const newVolume = ['On', 'On', 'On', 'On'];
     for (let i = parseInt(event.target.id); i < 4; i++)
       newVolume[i] = 'Off';
 
     this.setState({volume: newVolume});
-  }
+  };
+
+  classNameToNote = {
+    f3: 'white',
+    fis3: 'black',
+    g3: 'white l',
+    gis3: 'black',
+    a3: 'white l',
+    ais3: 'black',
+    h3: 'white l',
+    c4: 'white',
+    cis4: 'black',
+    d4: 'white l',
+    dis4: 'black',
+    e4: 'white l',
+    f4: 'white',
+    fis4: 'black',
+    g4: 'white l',
+    gis4: 'black',
+    a4: 'white l',
+    ais4: 'black',
+    h4: 'white l',
+    c5: 'white',
+    cis5: 'black',
+    d5: 'white l',
+    dis5: 'black',
+    e5: 'white l',
+    f5: 'white',
+    fis5: 'black',
+    g5: 'white l',
+    gis5: 'black',
+    a5: 'white l',
+    ais5: 'black',
+    h5: 'white l',
+    c6: 'white',
+    cis6: 'black',
+    d6: 'white l',
+    dis6: 'black',
+    e6: 'white l',
+  };
+
+  getClassNameToNote = (note) => {
+    if (this.state.clickedPianoKeys.includes(note)) {
+      return `${this.classNameToNote[note]} active`;
+    }
+    return this.classNameToNote[note];
+  };
 
   render() {
+    const signaturesClass = this.state.signatures ? 'On' : 'Off';
+    const signaturesRange = this.state.signatures ? '1' : '0';
+
     return (
       <ul className="Casing">
         <div className="options1">
-          <label className={this.state.infoSignatures}>Signatures</label><br/>
-          <input type="range" className="slider" value={this.state.signatures} min="0" max="1" step="1"
+          <label className={signaturesClass}>Signatures</label><br/>
+          <input type="range" className="slider" value={signaturesRange} min="0" max="1" step="1"
                  onChange={this.changeSignatures}/>
         </div>
         <div className="radio-group options2">
@@ -85,112 +133,112 @@ class Keyboard extends React.Component {
           <div id='4' className={this.state.volume[3]} onMouseDown={(e) => this.onSetVolume(e)}></div>
         </div>
         <br/>
-        <li id="f3" className="white" onMouseDown={(e) => this.play(e)}>
+        <li id="f3" className={this.getClassNameToNote('f3')} onMouseDown={this.play}>
           {this.state.keys[0]}
         </li>
-        <li id="fis3" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="fis3" className={this.getClassNameToNote('fis3')} onMouseDown={this.play}>
           {this.state.keys[21]}
         </li>
-        <li id="g3" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="g3" className={this.getClassNameToNote('g3')} onMouseDown={this.play}>
           {this.state.keys[1]}
         </li>
-        <li id="gis3" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="gis3" className={this.getClassNameToNote('gis3')} onMouseDown={this.play}>
           {this.state.keys[22]}
         </li>
-        <li id="a3" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="a3" className={this.getClassNameToNote('a3')} onMouseDown={this.play}>
           {this.state.keys[2]}
         </li>
-        <li id="ais3" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="ais3" className={this.getClassNameToNote('ais3')} onMouseDown={this.play}>
           {this.state.keys[23]}
         </li>
-        <li id="h3" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="h3" className={this.getClassNameToNote('h3')} onMouseDown={this.play}>
           {this.state.keys[3]}
         </li>
-        <li id="c4" className="white" onMouseDown={(e) => this.play(e)}>
+        <li id="c4" className={this.getClassNameToNote('c4')} onMouseDown={this.play}>
           {this.state.keys[4]}
         </li>
-        <li id="cis4" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="cis4" className={this.getClassNameToNote('cis4')} onMouseDown={this.play}>
           {this.state.keys[24]}
         </li>
-        <li id="d4" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="d4" className={this.getClassNameToNote('d4')} onMouseDown={this.play}>
           {this.state.keys[5]}
         </li>
-        <li id="dis4" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="dis4" className={this.getClassNameToNote('dis4')} onMouseDown={this.play}>
           {this.state.keys[25]}
         </li>
-        <li id="e4" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="e4" className={this.getClassNameToNote('e4')} onMouseDown={this.play}>
           {this.state.keys[6]}
         </li>
-        <li id="f4" className="white" onMouseDown={(e) => this.play(e)}>
+        <li id="f4" className={this.getClassNameToNote('f4')} onMouseDown={this.play}>
           {this.state.keys[7]}
         </li>
-        <li id="fis4" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="fis4" className={this.getClassNameToNote('fis4')} onMouseDown={this.play}>
           {this.state.keys[26]}
         </li>
-        <li id="g4" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="g4" className={this.getClassNameToNote('g4')} onMouseDown={this.play}>
           {this.state.keys[8]}
         </li>
-        <li id="gis4" className="black " onMouseDown={(e) => this.play(e)}>
+        <li id="gis4" className={this.getClassNameToNote('gis4')} onMouseDown={this.play}>
           {this.state.keys[27]}
         </li>
-        <li id="a4" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="a4" className={this.getClassNameToNote('a4')} onMouseDown={this.play}>
           {this.state.keys[9]}
         </li>
-        <li id="ais4" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="ais4" className={this.getClassNameToNote('ais4')} onMouseDown={this.play}>
           {this.state.keys[28]}
         </li>
-        <li id="h4" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="h4" className={this.getClassNameToNote('h4')} onMouseDown={this.play}>
           {this.state.keys[10]}
         </li>
-        <li id="c5" className="white " onMouseDown={(e) => this.play(e)}>
+        <li id="c5" className={this.getClassNameToNote('c5')} onMouseDown={this.play}>
           {this.state.keys[11]}
         </li>
-        <li id="cis5" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="cis5" className={this.getClassNameToNote('cis5')} onMouseDown={this.play}>
           {this.state.keys[29]}
         </li>
-        <li id="d5" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="d5" className={this.getClassNameToNote('d5')} onMouseDown={this.play}>
           {this.state.keys[12]}
         </li>
-        <li id="dis5" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="dis5" className={this.getClassNameToNote('dis5')} onMouseDown={this.play}>
           {this.state.keys[30]}
         </li>
-        <li id="e5" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="e5" className={this.getClassNameToNote('e5')} onMouseDown={this.play}>
           {this.state.keys[13]}
         </li>
-        <li id="f5" className="white" onMouseDown={(e) => this.play(e)}>
+        <li id="f5" className={this.getClassNameToNote('f5')} onMouseDown={this.play}>
           {this.state.keys[14]}
         </li>
-        <li id="fis5" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="fis5" className={this.getClassNameToNote('fis5')} onMouseDown={this.play}>
           {this.state.keys[31]}
         </li>
-        <li id="g5" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="g5" className={this.getClassNameToNote('g5')} onMouseDown={this.play}>
           {this.state.keys[15]}
         </li>
-        <li id="gis5" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="gis5" className={this.getClassNameToNote('gis5')} onMouseDown={this.play}>
           {this.state.keys[32]}
         </li>
-        <li id="a5" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="a5" className={this.getClassNameToNote('a5')} onMouseDown={this.play}>
           {this.state.keys[16]}
         </li>
-        <li id="ais5" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="ais5" className={this.getClassNameToNote('ais5')} onMouseDown={this.play}>
           {this.state.keys[33]}
         </li>
-        <li id="h5" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="h5" className={this.getClassNameToNote('h5')} onMouseDown={this.play}>
           {this.state.keys[17]}
         </li>
-        <li id="c6" className="white" onMouseDown={(e) => this.play(e)}>
+        <li id="c6" className={this.getClassNameToNote('c6')} onMouseDown={this.play}>
           {this.state.keys[18]}
         </li>
-        <li id="cis6" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="cis6" className={this.getClassNameToNote('cis6')} onMouseDown={this.play}>
           {this.state.keys[34]}
         </li>
-        <li id="d6" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="d6" className={this.getClassNameToNote('d6')} onMouseDown={this.play}>
           {this.state.keys[19]}
         </li>
-        <li id="dis6" className="black" onMouseDown={(e) => this.play(e)}>
+        <li id="dis6" className={this.getClassNameToNote('dis6')} onMouseDown={this.play}>
           {this.state.keys[35]}
         </li>
-        <li id="e6" className="white l" onMouseDown={(e) => this.play(e)}>
+        <li id="e6" className={this.getClassNameToNote('e6')} onMouseDown={this.play}>
           {this.state.keys[20]}
         </li>
       </ul>
